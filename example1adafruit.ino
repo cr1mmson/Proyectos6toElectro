761d8d3c800f8e67f67d942e3af88ef79f708c0e
Hans <57553501+cr1mmson@users.noreply.github.com>
2024-02-05 09:04:09 -0600

    Create example1adafruit.ino

---
+++ example1adafruit.ino
@@ -0,0 +1,35 @@
+#include "AdafruitIO_WiFi.h"
+
+#define IO_USERNAME  "USER"
+#define IO_KEY       "KEY"
+
+#define WIFI_SSID      "SSID"
+#define WIFI_PASS      "Password"
+
+AdafruitIO_Feed *gas = io.feed("ac");
+
+int ap=16;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Connecting to Adafruit IO");
+          io.connect();
+      while(io.status() < AIO_CONNECTED) {
+      Serial.print(".");
+      delay(500);
+    }
+
+    Serial.println();
+    Serial.println(io.statusText());
+}
+
+void loop() {
+  io.run();
+  envio_datos();
+}
+
+void envio_datos(){
+float por=digitalRead(ap);
+Serial.println(por);
+gas->save(por);
+}
